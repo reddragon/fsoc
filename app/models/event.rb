d198a4492b740a4ea6c05e899a3f1a7cb0226aa5
Gaurav Menghani <gaurav.menghani@gmail.com>
2010-07-07 01:32:04 +0530

    Calendar events in different colors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ActiveRecord::Base
   has_event_calendar
+  
+  def color
+    #You can add more colors, will be picked up uniformly
+    colors = ["#993333", "#003333", "#336600", "#996666", "#006666", \
+      "#336633", "#993366", "#660000", "#330066", "#003300"]
+    colors[id % colors.length]
+  end
 end
